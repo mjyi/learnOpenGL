95d7a125a6b5a268be49fd2877daaf55e96555df
Fish_ <liumy.f@outlook.com>
2019-02-19 16:11:05 +0800

    update  8.0

--- src/7.3.camera_class/camera_class.cpp
+++ src/7.3.camera_class/camera_class.cpp
@@ -32,11 +32,6 @@ void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-// camera
-glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
-
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
